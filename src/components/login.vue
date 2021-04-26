d025769ac68c57e357e991d232173155facfa059
katsuraDaCn <1650393312@qq.com>
2021-04-26 20:57:51 +0800

    1111

---
+++ src/components/login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="login_container">zzz</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style Lang="less" scoped>
+.login_container {
+    background-color: #2b4b6b;
+    height: 100%;
+}
+</style>
